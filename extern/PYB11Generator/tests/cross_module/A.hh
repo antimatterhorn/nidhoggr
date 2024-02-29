6acd3ab5c2dc07b47146a008f06525662ce6f64c
Cody Raskin <cody@antimatterhorn.com>
2024-02-29 00:56:41 -0800

    submodule

--- extern/PYB11Generator/tests/cross_module/A.hh
+++
@@ -1,5 +0,0 @@
-struct A {
-   A()                          { printf("A::A()\n"); }
-  ~A()                          { printf("A::~A()\n"); }
-  virtual int func(const int x) { printf("A::func(%d)\n", x); return x; }
-};
--- extern/PYB11Generator/tests/cross_module/B.hh
+++
@@ -1,7 +0,0 @@
-#include "A.hh"
-
-struct B: public A {
-  B(): A()                               { printf("B::B()\n"); }
-  ~B()                                   { printf("B::~B()\n"); }
-  virtual int func(const int x) override { printf("B::func(%d)\n", x); return x*x; }
-};
