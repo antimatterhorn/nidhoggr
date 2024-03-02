f560adc1e963d82860680ef592648bc31574c3c8
Cody Raskin <cody@antimatterhorn.com>
2024-03-01 18:28:54 -0800

    working pybind

--- src/RDE_PYB11.py
+++ src/RDE_PYB11.py
@@ -1,2 +1,10 @@
 from PYB11Generator import *
 PYB11includes = ['"grid.cpp"']
+
+class Grid:
+    def pyinit(self,n="int",size_x="int",size_y="int"):
+        return
+    def initialize(self):
+        return
+    def update(self):
+        return
