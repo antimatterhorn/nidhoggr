e858ae4f8123f398a767edd7643ba0eb4cd381cf
Cody Raskin <cody@antimatterhorn.com>
2024-04-16 09:33:01 -0700

    more python bindings for nodeList

--- src/DataBase/nodeList.py
+++ src/DataBase/nodeList.py
@@ -3,3 +3,8 @@ from PYB11Generator import *
 class NodeList:
     def pyinit(self):
         return
+    def pyinit1(self,numNodes="int"):
+        return
+    
+    numNodes = PYB11property("int", getter="getNumNodes", doc="The number of nodes in the nodeList.")
+    count = PYB11property("int", getter="getFieldListCount", doc="The number of fieldLists in the nodeList.")
