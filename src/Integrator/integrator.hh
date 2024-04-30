6891ff4d71e73a393d6869345cde1f65d0a45050
Cody Raskin <raskin1@llnl.gov>
2024-04-30 15:16:54 -0700

    integrator base class is forward euler

--- src/Integrator/integrator.hh
+++ src/Integrator/integrator.hh
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <vector>
+#include <cstddef>
 
 // Define a type alias for the derivative function
 using DerivativeFunction = std::function<std::vector<double>(double, const std::vector<double>&)>;
@@ -14,8 +15,30 @@ public:
     Integrator (const DerivativeFunction& derivativeFunc) : DxDt(derivativeFunc) {}
 
     ~Integrator() {}
+
+    virtual std::vector<double> integrate(std::vector<double> initialState, double t, double dt) {
+        // fwd euler
+        std::vector<double> nextState = addVectors(initialState,scalarMultiply(DxDt(t,initialState),dt));
+        return nextState;
+    }
+
+    std::vector<double> addVectors(const std::vector<double>& v1, const std::vector<double>& v2) {
+        std::vector<double> result;
+        for (size_t i = 0; i < v1.size(); ++i) {
+            result.push_back(v1[i] + v2[i]);
+        }
+        return result;
+    }
+
+    std::vector<double> scalarMultiply(const std::vector<double>& v, double scalar) {
+        std::vector<double> result;
+        for (double val : v) {
+            result.push_back(val * scalar);
+        }
+        return result;
+    }
 private:
-    
+
 
 };
 
--- src/Integrator/rungeKutta4Integrator.cc
+++ src/Integrator/rungeKutta4Integrator.cc
@@ -1,13 +1,13 @@
 
 #include "integrator.hh"
-#include <cstddef>
+
 
 class RungeKutta4Integrator : public Integrator {
 public:
     RungeKutta4Integrator(const DerivativeFunction& derivativeFunc) : Integrator(derivativeFunc) {}
 
     // Integrate method using Runge-Kutta 4
-    std::vector<double> integrate(std::vector<double> initialState, double t, double dt) {
+    std::vector<double> integrate(std::vector<double> initialState, double t, double dt) override {
         std::vector<double> k1 = this->DxDt(t, initialState);
         std::vector<double> k2 = this->DxDt(t + dt / 2, addVectors(initialState, scalarMultiply(k1, dt / 2)));
         std::vector<double> k3 = this->DxDt(t + dt / 2, addVectors(initialState, scalarMultiply(k2, dt / 2)));
@@ -21,19 +21,5 @@ public:
 private:
     
 
-    std::vector<double> addVectors(const std::vector<double>& v1, const std::vector<double>& v2) {
-        std::vector<double> result;
-        for (size_t i = 0; i < v1.size(); ++i) {
-            result.push_back(v1[i] + v2[i]);
-        }
-        return result;
-    }
 
-    std::vector<double> scalarMultiply(const std::vector<double>& v, double scalar) {
-        std::vector<double> result;
-        for (double val : v) {
-            result.push_back(val * scalar);
-        }
-        return result;
-    }
 };
