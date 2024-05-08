fdd2030008e40a492d1e7d1768954f3241a3abb6
Cody Raskin <cody@antimatterhorn.com>
2024-05-07 21:38:47 -0700

    finished rungeKutta4

--- src/Integrator/rungeKutta4Integrator.cc
+++ src/Integrator/rungeKutta4Integrator.cc
@@ -12,11 +12,12 @@ public:
     step(double dt) override {
         Physics<dim>* physics = this->physics;
         double time = this->time;
-        Field<double> k1,k2,k3,k4;
+        
         for (FieldBase* field : physics->derivFields) {
             if (typeid(*field) == typeid(Field<double>)) {
                 Field<double>* doubleField = dynamic_cast<Field<double>*>(field);
                 if (doubleField) {
+                    Field<double> k1,k2,k3,k4;
                     k1 = this->Derivative(doubleField,time,0);
                     Field<double> interim = Field<double>(doubleField->getNameString(),doubleField->size());
                     interim.copyValues(*doubleField + k1*(dt/2.0));
@@ -31,7 +32,17 @@ public:
             } else if (typeid(*field) == typeid(Field<Lin::Vector<dim>>)) {
                 Field<Lin::Vector<dim>>* vectorField = dynamic_cast<Field<Lin::Vector<dim>>*>(field);
                 if (vectorField) {
-                    // now do the same thing for vectors!!
+                    Field<Lin::Vector<dim>> k1,k2,k3,k4;
+                    k1 = this->Derivative(vectorField,time,0);
+                    Field<Lin::Vector<dim>> interim = Field<Lin::Vector<dim>>(vectorField->getNameString(),vectorField->size());
+                    interim.copyValues(*vectorField + k1*(dt/2.0));
+                    k2 = this->Derivative(&interim,time,dt/2.0);
+                    interim.copyValues(*vectorField + k2*(dt/2.0));
+                    k3 = this->Derivative(&interim,time,dt/2.0);
+                    interim.copyValues(*vectorField + k3*dt);
+                    k4 = this->Derivative(&interim,time,dt);
+                    Field<Lin::Vector<dim>> nextState = *vectorField + (k1 + k2*2.0 + k3*2.0 + k4) * (dt / 6.0);
+                    vectorField->copyValues(nextState);
                 }
             }
         }
