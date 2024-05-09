2f07d1b9589887ad323577faf4991bff188c2019
Cody Raskin <raskin1@llnl.gov>
2024-05-09 12:10:08 -0700

    tests cleanup

--- tests/vector_test.py
+++ tests/vector_test.py
@@ -4,14 +4,12 @@ if __name__ == "__main__":
     vec1 = Vector3d(1.0,2.0,3.0)
     vec2 = Vector3d(2.0,3.0,4.0)
 
-    print("a =",vec1)
-    print("b =",vec2)
-    print("a*b =",vec1*vec2)
-    print("axb =",vec1.cross(vec2))
-    print("a+b =",vec1+vec2)
-    print("a*2 =",vec1*2.0)
-    print("2*a =",2.0*vec1)
-    print("|a| =",vec1.magnitude)
-    print("a.norm =",vec1.normal())
-
-    import LinearAlgebra as lin
+    print("a\t=",   vec1)
+    print("b\t=",   vec2)
+    print("a*b\t=", vec1*vec2)
+    print("axb\t=", vec1.cross(vec2))
+    print("a+b\t=", vec1+vec2)
+    print("a*2\t=", vec1*2.0)
+    print("2*a\t=", 2.0*vec1)
+    print("|a|\t=", vec1.magnitude)
+    print("a.norm\t=",vec1.normal())
