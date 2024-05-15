6c17f7782a9b3ad6ac8548c7c66e18f84179e0d8
Cody Raskin <cody@antimatterhorn.com>
2024-05-14 23:11:19 -0700

    final fix before merge

--- src/Boundaries/Boundaries_PYB11.py
+++ src/Boundaries/Boundaries_PYB11.py
@@ -1,12 +1,12 @@
 from PYB11Generator import *
 PYB11includes = ['"boundaries.hh"',
                  '"gridBoundaries.hh"',
-                 #'"reflectingGridBoundaries.cc"',
+                 '"reflectingGridBoundaries.cc"',
                  '"pacmanGridBoundaries.cc"',
                  '"dirichletGridBoundaries.cc"']
 
 from boundaries import *
 from gridBoundaries import *
-#from reflectingGridBoundaries import *
+from reflectingGridBoundaries import *
 from pacmanGridBoundaries import *
 from dirichletGridBoundaries import *
