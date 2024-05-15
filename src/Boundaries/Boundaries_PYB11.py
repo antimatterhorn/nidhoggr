a098c5d2c86c2df7dbdbebb496945c077a750b62
Cody Raskin <cody@antimatterhorn.com>
2024-05-14 23:09:54 -0700

    restored pacman boundaries

--- src/Boundaries/Boundaries_PYB11.py
+++ src/Boundaries/Boundaries_PYB11.py
@@ -2,11 +2,11 @@ from PYB11Generator import *
 PYB11includes = ['"boundaries.hh"',
                  '"gridBoundaries.hh"',
                  #'"reflectingGridBoundaries.cc"',
-                 #'"pacmanGridBoundaries.cc"',
+                 '"pacmanGridBoundaries.cc"',
                  '"dirichletGridBoundaries.cc"']
 
 from boundaries import *
 from gridBoundaries import *
 #from reflectingGridBoundaries import *
-#from pacmanGridBoundaries import *
+from pacmanGridBoundaries import *
 from dirichletGridBoundaries import *
