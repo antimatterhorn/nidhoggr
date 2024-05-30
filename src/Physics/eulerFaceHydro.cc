9faebe5ec5a3ad67828d9059bc703eb47f0b9132
Cody Raskin <raskin1@llnl.gov>
2024-05-30 14:15:09 -0700

    beginning work on face-centered hydro instead of cell-centered

---
+++ src/Physics/eulerFaceHydro.cc
@@ -0,0 +1,149 @@
+#include "hydro.hh"
+#include "../Mesh/grid.hh"
+#include <iostream>
+
+template <int dim>
+class EulerFaceHydro : public Hydro<dim> {
+protected:
+    Mesh::Grid<dim>* grid;
+    std::vector<int> insideFaceIds; // IDs of interior faces
+public:
+    EulerFaceHydro() {}
+
+    EulerFaceHydro(NodeList* nodeList, PhysicalConstants& constants, EquationOfState* eos, Mesh::Grid<dim>* grid) :
+        Hydro<dim>(nodeList, constants, eos), grid(grid) {
+        VerifyEulerHydroFields(nodeList);
+
+        Field<double>* density = nodeList->getField<double>("density");
+        Field<Lin::Vector<dim>>* velocity = nodeList->getField<Lin::Vector<dim>>("velocity");
+        Field<double>* u = nodeList->getField<double>("specificInternalEnergy");
+
+        State<dim>* state = &this->state;
+        state->template addField<double>(density);
+        state->template addField<Lin::Vector<dim>>(velocity);
+        state->template addField<double>(u);
+
+        // Populate list of interior faces
+        for (int i = 0; i < grid->size(); ++i) {
+            if (!grid->onBoundary(i)) {
+                std::vector<int> faceIds = grid->getFaceIds(i);
+                insideFaceIds.insert(insideFaceIds.end(), faceIds.begin(), faceIds.end());
+            }
+        }
+    }
+
+    ~EulerFaceHydro() {}
+
+    virtual void
+    ZeroTimeInitialize() override {
+        NodeList* nodeList = this->nodeList;
+        Field<Lin::Vector<dim>>* v = nodeList->getField<Lin::Vector<dim>>("velocity");
+        Field<double>* rho = nodeList->getField<double>("density");
+        Field<double>* u = nodeList->getField<double>("specificInternalEnergy");
+
+        EOSLookup();
+    }
+
+    virtual void
+    VerifyEulerHydroFields(NodeList* nodeList) {
+        if (nodeList->getField<Lin::Vector<dim>>("position") == nullptr)
+            nodeList->insertField<Lin::Vector<dim>>("position");
+
+        Field<Lin::Vector<dim>>* position = nodeList->getField<Lin::Vector<dim>>("position");
+        for (int i = 0; i < position->size(); ++i) {
+            position->setValue(i, grid->getPosition(i));
+        }
+    }
+
+    virtual void EvaluateDerivatives(const State<dim>* initialState, State<dim>& deriv, const double time, const double dt) override {
+        NodeList* nodeList = this->nodeList;
+
+        Field<Lin::Vector<dim>>* velocity = initialState->template getField<Lin::Vector<dim>>("velocity");
+        Field<double>* density = initialState->template getField<double>("density");
+        Field<double>* drhodt = deriv.template getField<double>("density");
+        Field<double>* dudt = deriv.template getField<double>("specificInternalEnergy");
+        Field<Lin::Vector<dim>>* dvdt = deriv.template getField<Lin::Vector<dim>>("velocity");
+        Field<double>* pressure = nodeList->getField<double>("pressure");
+        Field<double>* soundSpeed = nodeList->getField<double>("soundSpeed");
+
+        double artificialViscosityCoefficient = 0.1; // Adjust as needed
+
+        for (int h = 0; h < insideFaceIds.size(); ++h) {
+            int faceId = insideFaceIds[h];
+            std::vector<int> cellIds = grid->getAdjacentCells(faceId);
+            int leftCellId = cellIds[0];
+            int rightCellId = cellIds[1];
+
+            double delDotV = 0.0;
+            Lin::Vector<dim> delP = Lin::Vector<dim>();
+
+            // Compute velocity gradient using upwind scheme
+            for (int d = 0; d < dim; ++d) {
+                double dvdx = (velocity->getValue(rightCellId)[d] - velocity->getValue(leftCellId)[d]) / grid->spacing(d);
+                delDotV += dvdx;
+
+                // Compute pressure gradient using upwind scheme
+                double dPdx = (pressure->getValue(rightCellId) - pressure->getValue(leftCellId)) / grid->spacing(d);
+                delP[d] = dPdx;
+
+                // Add artificial viscosity
+                double velocityDifference = velocity->getValue(rightCellId)[d] - velocity->getValue(leftCellId)[d];
+                delP[d] += artificialViscosityCoefficient * std::abs(velocityDifference) / grid->spacing(d);
+            }
+
+            double P = 0.5 * (pressure->getValue(leftCellId) + pressure->getValue(rightCellId));
+            double rho = 0.5 * (density->getValue(leftCellId) + density->getValue(rightCellId));
+            drhodt->setValue(faceId, -rho * delDotV);
+            dvdt->setValue(faceId, -delP / rho);
+            dudt->setValue(faceId, -P / rho * delDotV);
+        }
+    }
+
+    virtual void
+    FinalizeStep(const State<dim>* finalState) override {
+        NodeList* nodeList = this->nodeList;
+
+        Field<double>* fdensity = finalState->template getField<double>("density");
+        Field<Lin::Vector<dim>>* fvelocity = finalState->template getField<Lin::Vector<dim>>("velocity");
+        Field<double>* fu = finalState->template getField<double>("specificInternalEnergy");
+
+        Field<double>* density = nodeList->getField<double>("density");
+        Field<Lin::Vector<dim>>* velocity = nodeList->getField<Lin::Vector<dim>>("velocity");
+        Field<double>* u = nodeList->getField<double>("specificInternalEnergy");
+
+        density->copyValues(fdensity);
+        velocity->copyValues(fvelocity);
+        u->copyValues(fu);
+
+        EOSLookup();
+    }
+
+    virtual void
+    EOSLookup() {
+        NodeList* nodeList = this->nodeList;
+
+        Field<double>* rho = nodeList->getField<double>("density");
+        Field<double>* u = nodeList->getField<double>("specificInternalEnergy");
+        Field<double>* pressure = nodeList->getField<double>("pressure");
+        Field<double>* soundSpeed = nodeList->getField<double>("soundSpeed");
+
+        EquationOfState* eos = this->eos;
+        eos->setPressure(pressure, rho, u);
+        eos->setSoundSpeed(soundSpeed, rho, u);
+    }
+
+    double
+    getCell(int i, int j, std::string fieldName = "pressure") {
+        int idx = grid->index(i, j, 0);
+        NodeList* nodeList = this->nodeList;
+        Field<double>* pr = nodeList->getField<double>(fieldName);
+        return pr->getValue(idx);
+    }
+
+
+    virtual double
+    EstimateTimestep() const override {
+        // dt = dh/vmax
+        return 0;
+    }
+};
