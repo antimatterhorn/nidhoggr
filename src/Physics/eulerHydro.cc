06a1669ae29b2e825e1271f2bdfac807d3074bd9
Cody Raskin <raskin1@llnl.gov>
2024-05-30 14:01:50 -0700

    shortned the divergence calc in eulerHydro

--- src/Physics/eulerHydro.cc
+++ src/Physics/eulerHydro.cc
@@ -51,68 +51,53 @@ public:
         }
     }
 
-    virtual void
-    EvaluateDerivatives(const State<dim>* initialState, State<dim>& deriv, const double time, const double dt) override{  
-        // drhodt = - rho * del dot v
-        // dvdt = Forces - (del P) / rho
-        // dudt = -P/rho * del dot v       
+    virtual void EvaluateDerivatives(const State<dim>* initialState, State<dim>& deriv, const double time, const double dt) override {
         NodeList* nodeList = this->nodeList;
         int numNodes = nodeList->size();
 
-        Field<Lin::Vector<dim>>* velocity   = initialState->template getField<Lin::Vector<dim>>("velocity");
-        Field<double>* density              = initialState->template getField<double>("density");
-        Field<double>* drhodt               = deriv.template getField<double>("density");
-        Field<double>* dudt                 = deriv.template getField<double>("specificInternalEnergy");
-        Field<Lin::Vector<dim>>* dvdt       = deriv.template getField<Lin::Vector<dim>>("velocity");
-        Field<double>* pressure             = nodeList->getField<double>("pressure");
-        Field<double>* soundSpeed           = nodeList->getField<double>("soundSpeed");
+        Field<Lin::Vector<dim>>* velocity = initialState->template getField<Lin::Vector<dim>>("velocity");
+        Field<double>* density = initialState->template getField<double>("density");
+        Field<double>* drhodt = deriv.template getField<double>("density");
+        Field<double>* dudt = deriv.template getField<double>("specificInternalEnergy");
+        Field<Lin::Vector<dim>>* dvdt = deriv.template getField<Lin::Vector<dim>>("velocity");
+        Field<double>* pressure = nodeList->getField<double>("pressure");
+        Field<double>* soundSpeed = nodeList->getField<double>("soundSpeed");
 
-        for (int h=0; h<insideIds.size(); ++h) {
+        double artificialViscosityCoefficient = 0.1; // Adjust as needed
+
+        for (int h = 0; h < insideIds.size(); ++h) {
             int i = insideIds[h];
             std::vector<int> nbrs = grid->getNeighboringCells(i);
 
-            double delDotV;
-            double dvdx = velocity->getValue(nbrs[0])[0]-velocity->getValue(i)[0];
-            dvdx += velocity->getValue(i)[0] - velocity->getValue(nbrs[1])[0];
-            dvdx = dvdx*0.5/grid->dx;
-            delDotV = dvdx;
+            double delDotV = 0.0;
+            Lin::Vector<dim> delP = Lin::Vector<dim>();
 
-            Lin::Vector<dim> delP;
-            double dPdx = pressure->getValue(nbrs[0])-pressure->getValue(i);
-            dPdx += pressure->getValue(i)-pressure->getValue(nbrs[1]);
-            dPdx = dPdx*0.5/grid->dx;
-            delP[0] = dPdx;
+            // Compute divergence of velocity
+            for (int d = 0; d < dim; ++d) {
+                double dvdx = (velocity->getValue(nbrs[2 * d])[d] - velocity->getValue(i)[d]) +
+                            (velocity->getValue(i)[d] - velocity->getValue(nbrs[2 * d + 1])[d]);
+                dvdx = dvdx * 0.5 / grid->spacing(d);
+                delDotV += dvdx;
 
-            if (dim > 1) {
-                double dvdy = velocity->getValue(nbrs[2])[1]-velocity->getValue(i)[1];
-                dvdy += velocity->getValue(i)[1] - velocity->getValue(nbrs[3])[1];
-                dvdy = dvdy*0.5/grid->dy;
-                delDotV += dvdy;
+                // Compute pressure gradient
+                double dPdx = (pressure->getValue(nbrs[2 * d]) - pressure->getValue(i)) +
+                            (pressure->getValue(i) - pressure->getValue(nbrs[2 * d + 1]));
+                dPdx = dPdx * 0.5 / grid->spacing(d);
+                delP[d] = dPdx;
 
-                double dPdy = pressure->getValue(nbrs[2])-pressure->getValue(i);
-                dPdy += pressure->getValue(i)-pressure->getValue(nbrs[3]);
-                dPdy = dPdy*0.5/grid->dy;
-                delP[1] = dPdy;
-            }              
-            if (dim > 2) {
-                double dvdz = velocity->getValue(nbrs[4])[2]-velocity->getValue(i)[2];
-                dvdz += velocity->getValue(i)[2] - velocity->getValue(nbrs[5])[1];
-                dvdz = dvdz*0.5/grid->dz;
-                delDotV += dvdz;
-                
-                double dPdz = pressure->getValue(nbrs[4])-pressure->getValue(i);
-                dPdz += pressure->getValue(i)-pressure->getValue(nbrs[5]);
-                dPdz = dPdz*0.5/grid->dz;
-                delP[2] = dPdz;
+                // Add artificial viscosity
+                double velocityDifference = velocity->getValue(nbrs[2 * d])[d] - velocity->getValue(nbrs[2 * d + 1])[d];
+                delP[d] += artificialViscosityCoefficient * std::abs(velocityDifference) / grid->spacing(d);
             }
+
             double P = pressure->getValue(i);
             double rho = density->getValue(i);
-            drhodt->setValue(i,-rho*delDotV);
-            dvdt->setValue(i,-delP/rho);
-            dudt->setValue(i,-P/rho*delDotV);            
+            drhodt->setValue(i, -rho * delDotV);
+            dvdt->setValue(i, -delP / rho);
+            dudt->setValue(i, -P / rho * delDotV);
         }
-
     }
+
 
     virtual void
     FinalizeStep(const State<dim>* finalState) override {
