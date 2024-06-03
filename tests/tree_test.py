746f1104b6cb567215572919e77888abe1d2e89b
Cody Raskin <raskin1@llnl.gov>
2024-06-03 13:14:57 -0700

    made the tree test 3d

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -2,21 +2,23 @@ from nidhoggr import *
 
 nx = 10
 ny = 10
+nz = 10
 dx = 0.1
 dy = 0.1
+dz = 0.1
 
-nodeList = NodeList(nx*ny)
-myGrid = Grid2d(nx,ny,dx,dy)
+nodeList = NodeList(nx*ny*nz)
+myGrid = Grid3d(nx,ny,nz,dx,dy,dz)
 
 myGrid.assignPositions(nodeList)
 
-positions = nodeList.getFieldVector2d("position")
+positions = nodeList.getFieldVector3d("position")
 
-tree = KDTree2d(positions)
+tree = KDTree3d(positions)
 
-point = positions[int(nx/2*ny/2)]
-nbrs = tree.findNearestNeighbors(point,0.11)
-print(point,"is point %d"%int(nx/2*ny/2),"has neighbors",nbrs)
+point = positions[int(nx/2*ny/2*nz/2)]
+nbrs = tree.findNearestNeighbors(point,1.1*dx)
+print(point,"is point %d"%int(nx/2*ny/2*nz/2),"has neighbors",nbrs)
 for i in range(len(nbrs)):
     print(positions[nbrs[i]])
 
