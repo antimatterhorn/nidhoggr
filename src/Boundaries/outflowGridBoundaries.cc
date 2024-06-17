7325bacd475dffcf366e8e3fa76c1725299e0451
Cody Raskin <cody@antimatterhorn.com>
2024-06-16 23:33:38 -0700

    adding dim=3 version to outflow boundary

--- src/Boundaries/outflowGridBoundaries.cc
+++ src/Boundaries/outflowGridBoundaries.cc
@@ -56,6 +56,64 @@ public:
             boundaryIds.push_back(bottomIds);
             boundaryIds.push_back(bottomcol);
         }
+        else if (dim == 3) {
+            std::vector<int> leftIds   = grid->leftMost();  
+            std::vector<int> leftcol;
+
+            std::vector<int> rightIds  = grid->rightMost();
+            std::vector<int> rightcol;
+
+            std::vector<int> topIds    = grid->topMost();
+            std::vector<int> topcol;
+
+            std::vector<int> bottomIds = grid->bottomMost();
+            std::vector<int> bottomcol;
+
+            std::vector<int> frontIds  = grid->frontMost();
+            std::vector<int> frontcol;
+
+            std::vector<int> backIds   = grid->backMost();
+            std::vector<int> backcol;
+
+            int Nx = grid->size_x();
+            int Ny = grid->size_y();
+
+            for (int k = 0; k < Ny; ++k) {
+                for (int j = 0; j < Nx; ++j) {
+                    leftcol.push_back(grid->index(1, j, k));
+                    rightcol.push_back(grid->index(Nx - 2, j, k));
+                }
+            }
+
+            for (int k = 0; k < Ny; ++k) {
+                for (int i = 0; i < Nx; ++i) {
+                    topcol.push_back(grid->index(i, 1, k));
+                    bottomcol.push_back(grid->index(i, Ny - 2, k));
+                }
+            }
+
+            for (int j = 0; j < Nx; ++j) {
+                for (int i = 0; i < Ny; ++i) {
+                    frontcol.push_back(grid->index(i, j, 1));
+                    backcol.push_back(grid->index(i, j, Ny - 2));
+                }
+            }
+
+            boundaryIds.push_back(leftIds);
+            boundaryIds.push_back(leftcol);
+            boundaryIds.push_back(rightIds);
+            boundaryIds.push_back(rightcol);
+
+            boundaryIds.push_back(topIds);
+            boundaryIds.push_back(topcol);
+            boundaryIds.push_back(bottomIds);
+            boundaryIds.push_back(bottomcol);
+
+            boundaryIds.push_back(frontIds);
+            boundaryIds.push_back(frontcol);
+            boundaryIds.push_back(backIds);
+            boundaryIds.push_back(backcol);
+        }
     }
     
     virtual ~OutflowGridBoundaries() {}
