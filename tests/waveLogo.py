8ef7c08e832cfb0950ed7fbb8a80a983e2fedfd8
Cody Raskin <cody@antimatterhorn.com>
2025-04-01 23:12:18 -0700

    working on logo generation

--- tests/waveLogo.py
+++ tests/waveLogo.py
@@ -18,7 +18,7 @@ class oscillate:
         self.phi.setValue(idx,a)
 
 class SpkOscillator:
-    def __init__(self,speaker,nodeList,grid,width,height,workCycle=1):
+    def __init__(self,speaker,nodeList,grid,width,height,ns,workCycle=1):
         self.speaker = speaker
         self.nodeList = nodeList
         self.cycle = workCycle
@@ -26,13 +26,18 @@ class SpkOscillator:
         self.width = width
         self.height = height
         self.phi = self.nodeList.getFieldDouble("phi")
+        self.ns = ns
     def __call__(self,cycle,time,dt):
-        a = self.speaker.get_output(cycle*2.5e-3)
+        m = self.speaker.get_output(cycle*2.5e-3)
         #print(cycle*2.5e-3,a)
-        j = int(self.width/2)
-        i = int(self.height/2)
-        idx = self.grid.index(j,i,0)
-        self.phi.setValue(idx,a)
+        wa = self.width/self.ns
+        wb = self.height/self.ns
+        for a in range(self.ns):
+            for b in range(self.ns):
+                j = int(wa+a*wa)
+                i = int(wb+b*wb)
+                idx = self.grid.index(j,i,0)
+                self.phi.setValue(idx,m)
 
 from Utilities import HarmonicOscillator,SiloDump 
 
@@ -44,7 +49,8 @@ if __name__ == "__main__":
                                        ny = 100,
                                        cs = 1.0,
                                        frequency = 10.0,
-                                       amplitude  = 8.0)
+                                       amplitude  = 8.0,
+                                       ns = 5)
     
     constants = MKS()
 
@@ -66,22 +72,22 @@ if __name__ == "__main__":
     # --- 1. Left vertical bar ---
     x0 = int(nx * 0.1)
     x1 = int(nx * 0.2)
-    box.addBox(Vector2d(x0, 0), Vector2d(x1, ny))
+    box.addBox(Vector2d(x0, 0.1*ny), Vector2d(x1, 0.9*ny))
 
     # --- 2. Right vertical bar ---
     x2 = int(nx * 0.8)
     x3 = int(nx * 0.9)
-    box.addBox(Vector2d(x2, 0), Vector2d(x3, ny))
+    box.addBox(Vector2d(x2, 0.1*ny), Vector2d(x3, 0.9*ny))
 
     # --- 3. Diagonal bar approximation ---
     # We'll make thin boxes that step diagonally upward
-    num_steps = ny
+    num_steps = int(0.8*ny)
     for step in range(num_steps):
         y0 = step
         y1 = step + 1
         # Interpolate x from left to right as y increases
         x = int(x2 - (x2 - x0) * (step / num_steps))
-        box.addBox(Vector2d(x, y0), Vector2d(x + 1, y1))
+        box.addBox(Vector2d(x, y0+0.1*ny), Vector2d(x + 8, y1+0.1*ny))
     box.addDomain()
 
     waveEqn.addBoundary(box)
@@ -94,7 +100,7 @@ if __name__ == "__main__":
 
     #osc = oscillate(nodeList=myNodeList,grid=grid,width=nx,height=ny,workCycle=1)
     spk = HarmonicOscillator(frequency=frequency,amplitude=amplitude)
-    osc = SpkOscillator(nodeList=myNodeList,grid=grid,width=nx,height=ny,workCycle=1,speaker=spk)
+    osc = SpkOscillator(nodeList=myNodeList,grid=grid,width=nx,height=ny,ns=ns,workCycle=1,speaker=spk)
     periodicWork = [osc]
     if(dump):
         silo = SiloDump("testMesh",myNodeList,fieldNames=["phi","xi"],dumpCycle=50)
