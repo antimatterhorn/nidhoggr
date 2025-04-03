f1a6e16ef3c3e5f3c0e992959a828af0b60fcb8d
Cody Raskin <raskin1@llnl.gov>
2025-04-03 10:50:05 -0700

    moving element factory function into element.hh and including fem in compilation

--- src/Mesh/element.hh
+++ src/Mesh/element.hh
@@ -61,5 +61,23 @@ namespace Mesh {
         }
     };
         
-    
+    template <int dim>
+    std::shared_ptr<Element<dim>> 
+    createElement(ElementType type, const std::vector<size_t>& nodeIndices) {
+        if constexpr (dim == 2) {
+            switch (type) {
+                case ElementType::Triangle:
+                    return std::make_shared<TriangleElement>(nodeIndices);
+                case ElementType::Quad:
+                    return std::make_shared<QuadElement>(nodeIndices);
+                default:
+                    throw std::invalid_argument("Unsupported ElementType for 2D mesh");
+            }
+        } else if constexpr (dim == 3) {
+            // add 3D support here later
+            throw std::invalid_argument("3D elements not implemented yet");
+        } else {
+            throw std::invalid_argument("Unsupported spatial dimension");
+        }
+    }    
 }
--- src/Mesh/femesh.py
+++ src/Mesh/femesh.py
@@ -2,5 +2,9 @@ from PYB11Generator import *
 
 @PYB11template("dim")
 class FEMesh:
-    def pyinit(self)
+    def pyinit(self):
         return
+    def addNode(self,position="Lin::Vector<%(dim)s>"):
+        return
+    def getNodes(self):
+        return "std::vector<Lin::Vector<dim>>"
