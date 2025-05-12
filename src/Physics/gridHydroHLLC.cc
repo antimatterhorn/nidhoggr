4792261c8ff90950f3628e99b7de31e3b9bd381a
Cody Raskin <raskin1@llnl.gov>
2025-05-12 14:56:10 -0700

    explicitly labeling HLL solver as HLLE and beginning work on HLLC

--- src/Physics/gridHydroHLLC.cc
+++ src/Physics/gridHydroHLLC.cc
@@ -1,87 +1,10 @@
 #include "hydro.hh"
 #include "../Mesh/grid.hh"
+#include "HLL.cc"
 #include <iostream>
 
-template<int dim>
-struct HLLFlux {
-    double mass;
-    Lin::Vector<dim> momentum;
-    double energy;
-};
-
-template<int dim>
-HLLFlux<dim>
-computeHLLFlux(int iL, int iR, int axis,
-               const Field<double>& rho,
-               const Field<Lin::Vector<dim>>& v,
-               const Field<double>& u,
-               const Field<double>& p,
-               const Field<double>& cs) {
-    using Vector = Lin::Vector<dim>;
-
-    // Left state
-    double rhoL = rho.getValue(iL);
-    Vector vL = v.getValue(iL);
-    double uL = u.getValue(iL);
-    double pL = p.getValue(iL);
-    double cL = cs.getValue(iL);
-    Vector momL = vL * rhoL;
-    double eL = uL + 0.5 * vL.mag2();
-    double EL = rhoL * eL;
-
-    // Right state
-    double rhoR = rho.getValue(iR);
-    Vector vR = v.getValue(iR);
-    double uR = u.getValue(iR);
-    double pR = p.getValue(iR);
-    double cR = cs.getValue(iR);
-    Vector momR = vR * rhoR;
-    double eR = uR + 0.5 * vR.mag2();
-    double ER = rhoR * eR;
-
-    // Normal velocities
-    double vnL = vL[axis];
-    double vnR = vR[axis];
-
-    // Fluxes for left and right states
-    double massFluxL = rhoL * vnL;
-    Vector momFluxL = momL * vnL;
-    momFluxL[axis] += pL;
-    double energyFluxL = vnL * (EL + pL);
-
-    double massFluxR = rhoR * vnR;
-    Vector momFluxR = momR * vnR;
-    momFluxR[axis] += pR;
-    double energyFluxR = vnR * (ER + pR);
-
-    // Estimate wave speeds
-    double sL = std::min(vnL - cL, vnR - cR);
-    double sR = std::max(vnL + cL, vnR + cR);
-
-    // HLL flux
-    HLLFlux<dim> result;
-
-    if (sL >= 0.0) {
-        result.mass = massFluxL;
-        result.momentum = momFluxL;
-        result.energy = energyFluxL;
-    } else if (sR <= 0.0) {
-        result.mass = massFluxR;
-        result.momentum = momFluxR;
-        result.energy = energyFluxR;
-    } else {
-        double sRSum = sR - sL;
-        result.mass     = (sR * massFluxL - sL * massFluxR + sR * sL * (rhoR - rhoL)) / sRSum;
-        result.momentum = (sR * momFluxL - sL * momFluxR + sR * sL * (momR - momL)) / sRSum;
-        result.energy   = (sR * energyFluxL - sL * energyFluxR + sR * sL * (ER - EL)) / sRSum;
-    }
-
-    return result;
-}
-
-
 template <int dim>
-class GridHydroHLL : public Hydro<dim> {
+class GridHydroHLLC : public Hydro<dim> {
 protected:
     Mesh::Grid<dim>* grid;
     std::vector<int> insideIds;
@@ -91,7 +14,7 @@ public:
     using VectorField = Field<Vector>;
     using ScalarField = Field<double>;
 
-    GridHydroHLL(NodeList* nodeList, PhysicalConstants& constants, EquationOfState* eos, Mesh::Grid<dim>* grid) : 
+    GridHydroHLLC(NodeList* nodeList, PhysicalConstants& constants, EquationOfState* eos, Mesh::Grid<dim>* grid) : 
         Hydro<dim>(nodeList, constants, eos), grid(grid) {
         std::cout << grid->size() << std::endl;
         VerifyHLLFields(nodeList);
@@ -119,7 +42,7 @@ public:
             dxmin = std::min(dxmin,grid->spacing(i));
     }
 
-    ~GridHydroHLL() {}
+    ~GridHydroHLLC() {}
 
     virtual void 
     ZeroTimeInitialize() override {
@@ -183,8 +106,8 @@ public:
                 int jL = neighbors[2 * k];       // left neighbor in dimension k
                 int jR = neighbors[2 * k + 1];   // right neighbor in dimension k
 
-                auto flux_L = computeHLLFlux<dim>(jL, i, k, *rho, *v, *u, *pressure, *soundSpeed);
-                auto flux_R = computeHLLFlux<dim>(i, jR, k, *rho, *v, *u, *pressure, *soundSpeed);
+                auto flux_L = computeHLLCFlux<dim>(jL, i, k, *rho, *v, *u, *pressure, *soundSpeed);
+                auto flux_R = computeHLLCFlux<dim>(i, jR, k, *rho, *v, *u, *pressure, *soundSpeed);
 
                 double dx = grid->spacing(k);
 
